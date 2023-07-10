4f5abb09aef8d6b58228e2e23c7e95492b98e1d1
nmkha-gitlab <nguyenminhkha2001@gmail.com>
2023-07-10 23:01:43 +0700

    update homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,98 +1,217 @@
-import React from "react";
-import { Box, Button, Tooltip, Typography } from "@mui/material";
-import APP_LOGO from "../../modules/layout/constants/app-logo";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import { motion } from "framer-motion";
+import { FiInfo } from "react-icons/fi";
+import { BiMoviePlay } from "react-icons/bi";
+import heroImage from "../../assets/images/hero-img.png";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const [showTooltip, setShowTooltip] = useState(false);
   const navigate = useNavigate();
-  const location = useLocation();
 
   return (
-    <Box
-      style={{
-        width: "100%",
-        height: "100vh",
-        background:
-          "linear-gradient(90deg, rgba(0,212,255,1) 49%, rgba(7,255,255,1) 100%, rgba(2,0,36,1) 100%)",
-      }}
-    >
-      <Tooltip title="nmcnpm-group24.vercel.app">
+    <Box style={{ height: "100vh", background: "#37517e" }}>
+      <Box style={{ paddingTop: 12, marginLeft: "10%", marginRight: "10%" }}>
+        {/* Header */}
         <Box
           style={{
             display: "flex",
+            justifyContent: "space-between",
             alignItems: "center",
-            width: 260,
-            paddingTop: 4,
-            paddingLeft: 16,
-            position: "absolute",
           }}
-          onClick={() =>
-            !location.pathname.includes("/home") && navigate("/home")
-          }
         >
-          <img
-            style={{ width: 60, height: 60 }}
-            src={APP_LOGO}
-            alt="web logo"
-          />
-
           <Typography
             style={{
               fontFamily: "cursive",
               color: "white",
-              fontStyle: "italic",
-              fontSize: 40,
+              fontWeight: "bold",
+            }}
+            variant="h4"
+          >
+            TRANSFORMER
+          </Typography>
+
+          <Box
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
             }}
           >
-            Taskment
-          </Typography>
+            <Box
+              style={{
+                position: "relative",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <FiInfo
+                size={28}
+                style={{ color: "white" }}
+                onMouseEnter={() => setShowTooltip(true)}
+                onMouseLeave={() => setShowTooltip(false)}
+              />
+              {showTooltip && (
+                <Box
+                  style={{
+                    position: "absolute",
+                    width: 240,
+                    borderRadius: 16,
+                    padding: "12px 16px",
+                    background: "white",
+                    marginTop: 40,
+                    left: -120,
+                    zIndex: 99999,
+                  }}
+                >
+                  <Box>
+                    <Typography
+                      style={{ display: "flex", justifyContent: "center" }}
+                      fontWeight="bold"
+                    >
+                      Giới thiệu
+                    </Typography>
+                    <Typography>
+                      Transformer là một mô hình đã tạo nên bước ngoặt lớn trong
+                      việc xử lý ngôn ngữ tự nhiên ban đầu và được sử dụng rộng
+                      rãi ngày nay mô hình RoBERTa (Robustly Optimized BERT
+                      Approach), ra đời năm 2019 bởi Facebook AI , một mô hình
+                      được tối ưu dựa trên mô hình BERT được ra đời năm 2018 bởi
+                      Google.
+                    </Typography>
+                  </Box>
+                </Box>
+              )}
+            </Box>
+            <motion.button
+              whileHover={{ background: "#47b2e4" }}
+              style={{
+                marginLeft: 16,
+                padding: "10px 24px 11px 24px",
+                fontSize: 16,
+                background: "none",
+                fontFamily: "sans-serif",
+                fontWeight: "bold",
+                color: "white",
+                border: "2px solid #47b2e4",
+                borderRadius: 50,
+                cursor: "pointer",
+              }}
+              onClick={() => navigate("/login")}
+            >
+              Bắt Đầu
+            </motion.button>
+          </Box>
         </Box>
-      </Tooltip>
-
-      <Box style={{ display: "flex", flexDirection: "row-reverse" }}>
-        <Button
-          variant="contained"
-          color="primary"
-          style={{ textTransform: "none", margin: "16px 4px" }}
-          onClick={() => navigate("/register")}
-        >
-          <Typography style={{ fontWeight: 600 }}>Đăng ký</Typography>
-        </Button>
-
-        <Button
-          variant="text"
-          color="inherit"
-          style={{ textTransform: "none", margin: "16px 4px" }}
-          onClick={() => navigate("/login")}
-        >
-          <Typography style={{ fontWeight: 600, color: "white" }}>
-            Đăng nhập
-          </Typography>
-        </Button>
-      </Box>
-
-      <Box style={{ display: "flex", height: "calc(100vh - 68px)" }}>
-        <Box
-          style={{
-            flex: 1,
-            position: "relative",
-            top: "28%",
+        {/* Body */}
+        <motion.div
+          animate={{
+            opacity: [0, 0.5, 1],
+            y: [20, 0],
           }}
-        >
-          <Typography variant="h2" align="center" style={{ color: "white" }}>
-            Taskment
-          </Typography>
-          <Typography align="center" style={{ color: "white" }}>
-            Ứng dụng giúp quản lý công việc hiệu quả
-          </Typography>
-        </Box>
-        <Box
-          style={{
-            flex: 2,
+          transition={{
+            duration: 2,
+            ease: "easeInOut",
           }}
+          style={{ height: "calc(100vh - 54,6px)", display: "flex" }}
         >
-          {/*  */}
-        </Box>
+          <Box
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              flex: 1,
+              color: "white",
+            }}
+          >
+            <Box
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Typography variant="h3" fontWeight="bold">
+                Question Answering With Transformer
+              </Typography>
+              <Typography variant="h5" color="rgba(255, 255, 255, 0.6)">
+                Mô hình RoBERTa
+              </Typography>
+
+              <Box
+                style={{ marginTop: 16, display: "flex", alignItems: "center" }}
+              >
+                <BiMoviePlay size={32} />
+                <Box style={{ width: 8 }} />
+                <Box>
+                  <motion.button
+                    whileHover={{ border: "2px solid #B0E0E6" }}
+                    style={{
+                      padding: "10px 24px 11px 24px",
+                      fontSize: 16,
+                      background: "none",
+                      fontFamily: "sans-serif",
+                      fontWeight: "bold",
+                      color: "white",
+                      border: "2px solid #47b2e4",
+                      borderRadius: 50,
+                      cursor: "pointer",
+                    }}
+                    onClick={() =>
+                      window.open(
+                        "https://www.youtube.com/watch?v=SuqU904ZHA4&ab_channel=Fireship"
+                      )
+                    }
+                  >
+                    Xem Demo
+                  </motion.button>
+                </Box>
+              </Box>
+
+              <Box style={{ marginTop: 16, display: "flex" }}>
+                <motion.button
+                  whileHover={{ background: "#209dd8" }}
+                  style={{
+                    padding: "16px 28px 16px 28px",
+                    fontSize: 24,
+                    background: "#47b2e4",
+                    fontFamily: "sans-serif",
+                    fontWeight: "bold",
+                    color: "white",
+                    border: "none",
+                    borderRadius: 50,
+                    cursor: "pointer",
+                  }}
+                  onClick={() => navigate("/login")}
+                >
+                  Bắt Đầu Ngay
+                </motion.button>
+              </Box>
+            </Box>
+          </Box>
+
+          <Box
+            style={{
+              minHeight: 600,
+              display: "flex",
+              flex: 1,
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <motion.div
+              animate={{ y: [-12, 12, -12] }}
+              transition={{ repeat: Infinity, duration: 2 }}
+            >
+              <img
+                alt=""
+                src={heroImage}
+                style={{ objectFit: "cover", width: "100%" }}
+              />
+            </motion.div>
+          </Box>
+        </motion.div>
       </Box>
     </Box>
   );
