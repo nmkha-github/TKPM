02e53e20a42c96d3ec393b3d685391285bbcd741
QuangHuy54 <73436308+QuangHuy54@users.noreply.github.com>
2023-07-14 19:16:40 +0700

    Conflict

--- src/pages/SchedulePage/SchedulePage.tsx
+++ src/pages/SchedulePage/SchedulePage.tsx
@@ -18,6 +18,7 @@ import {
   Grid,
   Typography,
 } from "@mui/material";
+import TaskData from "../../modules/task/interface/task-data";
 import dayjs, { Dayjs } from "dayjs";
 import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -39,44 +40,81 @@ import {
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment";
+import { useTasks } from "../../lib/provider/TasksProvider";
+import { unionTypeAnnotation } from "@babel/types";
 const events = [
   {
-    id: 0,
     title: "All Day Event very long title",
     allDay: true,
     start: new Date(2023, 7, 7),
     end: new Date(2023, 7, 7),
   },
   {
-    id: 1,
     title: "Long Event",
     start: new Date(2023, 3, 7),
     end: new Date(2023, 3, 10),
   },
 
   {
-    id: 2,
     title: "DTS STARTS",
     start: new Date(2016, 2, 13, 0, 0, 0),
     end: new Date(2016, 2, 20, 0, 0, 0),
   },
 ];
+interface Event {
+  title: string;
+  start: Date;
+  end: Date;
+}
 const localizer = momentLocalizer(moment);
 const DragAndDropCalendar = withDragAndDrop(Calendar);
 const SchedulePage = () => {
-  const [myEvents, setMyEvents] = useState(events);
+  const [myEvents, setMyEvents] = useState<any[]>([]);
   const handleSelectEvent = useCallback(
     (event) => window.alert(event.title),
     []
   );
   const { rooms, getRooms, loadingRooms, loadingMoreRooms, loadedAllRooms } =
     useRooms();
-  console.log(rooms);
+  const [TotalTasks, setTotalTasks] = useState<TaskData[]>([]);
+  const {
+    tasks,
+    getTasks,
+  } = useTasks();
+  useEffect(() => {
+    getRooms({ getLimit:9999 });
+  }, []);
+  useEffect(() => {
+    if(rooms){
+      rooms.forEach(async (room)=>{
+        await getTasks({room_id:room.id})
+        const new_total_task=[...TotalTasks]
+        tasks.forEach(value=>{
+          if(!TotalTasks.includes(value)&&value.deadline){
+            TotalTasks.push(value)
+          }
+        })
+        setTotalTasks([...new_total_task])
+      })
+    }
+  }, [rooms]);
+  useEffect(() => {
+    console.log(TotalTasks)
+    setMyEvents(TotalTasks.map((task)=>({
+      title: task.title,
+      start: task.deadline,
+      allDay: true,
+      end: task.deadline,
+    })))
+  }, [TotalTasks]);
+  useEffect(() => {
+    console.log(myEvents)
+  }, [myEvents]);
   return (
     <Fragment>
       <Calendar
         localizer={localizer}
-        events={events}
+        events={myEvents}
         onSelectEvent={handleSelectEvent}
         startAccessor="start"
         endAccessor="end"
